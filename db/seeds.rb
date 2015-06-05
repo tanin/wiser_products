9ba99edd7264eeb9b54d7d4f31c279496e066e92
Daniel Lurie <daniel@showbox.com>
2015-06-05 23:18:39 +0300

    added categories to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 CATEGORIES = %w(red green blue grey)
 
+CATEGORIES.each do |category|
+  Category.create!(name: category)
+end
+
 (1..1000).step(1) do |i|
   Product.create!(
     name: "name_#{i}",
     sku: rand(36**6).to_s(36),
-    category: CATEGORIES.sample
+    category: Category.order("RANDOM()").first
   )
 end
