d0d6079c7d714ee0598643892b6d68f1645aa980
Daniel Lurie <daniel@showbox.com>
2015-06-05 20:31:11 +0300

    seed db

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 32, too_long: "%{count} characters is the maximum allowed" }
-  validates :sku, presence: true, uniqueness: true, length: { maximum: 6, too_long: "%{count} characters is the maximum allowed" } #XXX uniqness
+  validates :sku, presence: true, uniqueness: true, length: { maximum: 6, too_long: "%{count} characters is the maximum allowed" }
   validates :category, presence: true, length: { maximum: 10, too_long: "%{count} characters is the maximum allowed" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+CATEGORIES = %w(red green blue grey)
+
+(1..1000).step(1) do |i|
+  Product.create!(
+    name: "name_#{i}",
+    sku: rand(36**6).to_s(36),
+    category: CATEGORIES.sample
+  )
+end
